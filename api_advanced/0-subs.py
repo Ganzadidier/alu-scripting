ea941cc303ae334b6e89e1f19cf8546a794d0fa1
Ganzadidier <d.ganza@alustudent.com>
2024-07-04 21:04:51 +0000

    adding try and except condition

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,9 +2,8 @@
 """
 Contains the number_of_subscribers function
 """
-
+import json
 import requests
-
 
 def number_of_subscribers(subreddit):
     # Define the URL for the subreddit's about endpoint
@@ -19,13 +18,22 @@ def number_of_subscribers(subreddit):
         
         # Check if the status code indicates a successful request
         if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Return "OK" and the number of subscribers
-            return "OK", data['data']['subscribers']
+            try:
+                # Parse the JSON response
+                data = response.json()
+                # Check if the key 'data' exists in the JSON response
+                if 'data' in data and 'subscribers' in data['data']:
+                    # Return the number of subscribers
+                    return data['data']['subscribers']
+                else:
+                    # If the 'data' or 'subscribers' key is not present, return 0
+                    return 0
+            except ValueError:
+                # If there is a JSON decoding error, return 0
+                return 0
         else:
             # If the subreddit does not exist or any other error, return 0
-            return "0"
+            return 0
     except requests.RequestException:
         # In case of a network error or any other issue, return 0
-        return "0"
+        return 0
